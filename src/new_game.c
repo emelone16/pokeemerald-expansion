8b14cda351672e8852b53d783f92a395ab452d5f
emelone16 <erikjmelone@gmail.com>
2024-02-08 23:21:59 -0500

    Change spawn to Celestic ruins

--- src/new_game.c
+++ src/new_game.c
@@ -126,7 +126,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
+    SetWarpDestination(MAP_GROUP(CELESTIC_TOWN_RUINS), MAP_NUM(CELESTIC_TOWN_RUINS), 1, -1, -1);
     WarpIntoMap();
 }
 
